5256993af0bdf6682bcc04d9b5b62791753a0526
Ticki <Ticki@users.noreply.github.com>
2016-03-18 10:00:52 +0100

    Add done screen

--- src/ice/main.rs
+++ src/ice/main.rs
@@ -9,6 +9,7 @@ const MAP_1: &'static [u8] = include_bytes!("map1.txt");
 const MAP_2: &'static [u8] = include_bytes!("map2.txt");
 const MAP_3: &'static [u8] = include_bytes!("map3.txt");
 const MAP_4: &'static [u8] = include_bytes!("map4.txt");
+const DONE: &'static [u8] = include_bytes!("done.txt");
 
 /// The game state.
 struct Game<R, W: Write> {
@@ -150,7 +151,7 @@ impl<R: Read, W: Write> Game<R, W> {
             1 => MAP_2,
             2 => MAP_3,
             3 => MAP_4,
-            _ => unimplemented!(),
+            _ => DONE,
         }
     }
 
